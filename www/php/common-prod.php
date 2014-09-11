6b1f8d44805d31d027de136ddd9b390342ce2842
Jesper Baasch-Larsen <jla@fcoo.dk>
2014-09-11 15:15:34 +0200

    not merging Locate control

--- www/php/common-prod.php
+++ www/php/common-prod.php
@@ -6,9 +6,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" type="text/css" href="ifm-maps.css" />
     <link rel="stylesheet" type="text/css" href="../fonts/font-awesome-4.1.0/css/font-awesome.min.css" />
+    <link href='../javascript/leaflet/leaflet-locatecontrol_2014-09-11/L.Control.Locate.css' rel='stylesheet' />
 </head>
 <body>
     <div id="map"></div>
+    <script src='../javascript/leaflet/leaflet-locatecontrol_2014-09-11/L.Control.Locate.js'></script>
     <script src="ifm-maps.js"></script>
     <script>window.L_PREFER_CANVAS = true;</script>
     <!--[if lt IE 9]><script type="text/javascript" src="../javascript/polyfills/excanvas.js"></script><![endif]-->
