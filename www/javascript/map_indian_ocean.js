2fe015d10909f82ec82266b9de614ad93dbe660a
Jesper Baasch-Larsen <jla@fcoo.dk>
2014-11-13 12:18:45 +0100

    added dbpedia

--- www/javascript/map_indian_ocean.js
+++ www/javascript/map_indian_ocean.js
@@ -26,6 +26,9 @@ function initMap() {
         "boundaries": {
             "EEZ": new L.tileLayer(fcoo_base + "tiles_EEZ_" + tilesize + "_mercator_201411070000" + "/{z}/{x}/{y}.png",
 		 {maxZoom: 10, tileSize: tilesize, subdomains: subdomains, zIndex: 200, continuousWorld: false, errorTileUrl: fcoo_base + "empty_" + tilesize +".png"}),
+        },
+        "places": {
+            "DBPedia (experimental)": L.dbPediaLayer({lang: 'en', includeCities: true, minZoom: 8}),
         }
     }
     var minZoom = 4;
