9b66ce039863f17641c3b756f51f906c4227f089
Jesper Baasch-Larsen <jla@fcoo.dk>
2015-04-24 13:31:00 +0200

    fixed cutycapt could not parse iso date

--- javascript/map_common.js
+++ javascript/map_common.js
@@ -238,7 +238,11 @@
         // Initialize datetime control with this time if in URL
         var initial_datetime;
         if (urlParams.datetime !== undefined) {
-            initial_datetime = new Date(window.unescape(urlParams.datetime));
+            var res = window.unescape(urlParams.datetime).split('T');
+            var res1 = res[0].split('-');
+            var res2 = res[1].split(':');
+            initial_datetime = new Date(res1[0], res1[1], res1[2], res2[0], res2[1], res2[2]);
+            //initial_datetime = new Date(window.unescape(urlParams.datetime));
         } else {
             initial_datetime = null;
         }
