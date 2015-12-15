e1d3f0180e405aa82ff42d06535c0901f6b6f3da
Jesper Baasch-Larsen <jla@fcoo.dk>
2015-12-15 19:29:13 +0100

    fixed issue with leaflet and event interface polyfill

--- javascript/ie8polyfill.js
+++ javascript/ie8polyfill.js
@@ -164,32 +164,3 @@ if ( !Date.prototype.toISOString ) {
         };       
     }() );
 }
-
-!window.addEventListener && (function (WindowPrototype, DocumentPrototype, ElementPrototype, addEventListener, removeEventListener, dispatchEvent, registry) {
-	WindowPrototype[addEventListener] = DocumentPrototype[addEventListener] = ElementPrototype[addEventListener] = function (type, listener) {
-		var target = this;
-
-		registry.unshift([target, type, listener, function (evt) {
-			evt.currentTarget = target;
-			evt.prevt = function () { evt.returnValue = false; };
-			evt.stopPropagation = function () { evt.cancelBubble = true; };
-			evt.target = evt.srcElement || target;
-
-			listener.call(target, evt);
-		}]);
-
-		this.attachEvent("on" + type, registry[0][3]);
-	};
-
-	WindowPrototype[removeEventListener] = DocumentPrototype[removeEventListener] = ElementPrototype[removeEventListener] = function (type, listener) {
-		for (var index = 0, register; register = registry[index]; ++index) {
-			if (register[0] == this && register[1] == type && register[2] == listener) {
-				return this.detachEvent("on" + type, registry.splice(index, 1)[0][3]);
-			}
-		}
-	};
-
-	WindowPrototype[dispatchEvent] = DocumentPrototype[dispatchEvent] = ElementPrototype[dispatchEvent] = function (eventObject) {
-		return this.fireEvent("on" + eventObject.type, eventObject);
-	};
-})(Window.prototype, HTMLDocument.prototype, Element.prototype, "addEventListener", "removeEventListener", "dispatchEvent", []);
